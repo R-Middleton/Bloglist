945e4c0275e37ebcd8cebf198ee6c6904095a3d9
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-01-21 09:52:44 +0000

    fixed blog post and added rest requests

--- controller/blogs.js
+++ controller/blogs.js
@@ -2,13 +2,23 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (request, response) => {
+  console.log('blog', response.body)
+
   Blog.find({}).then((blogs) => {
     response.json(blogs)
   })
 })
 
-blogsRouter.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', (request, response) => {
+  const body = request.body
+  console.log('new blog', body)
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
 
   blog.save().then((result) => {
     response.status(201).json(result)
