e2707d380dad487f396d1f2c61ebcd76a6d46dd5
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-01-24 22:41:57 +0000

    Added 2 tests from 'supertest' section

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -11,6 +11,18 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
+test('there are four blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(4)
+})
+
+test('the first blog is about React patterns', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].title).toBe('React patterns')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
