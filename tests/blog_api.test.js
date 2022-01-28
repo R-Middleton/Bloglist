1c96ae17c34a1571e24d463572356a0ffff47f0f
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-01-28 19:05:01 +0000

    changed tests to run on an initialsed set of test data

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,47 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const { init } = require('../models/blog')
+const blog = require('../models/blog')
 const api = supertest(app)
+const Blog = require('../models/blog')
+const initalBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html',
+    likes: 10,
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+  },
+]
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initalBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initalBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(initalBlogs[2])
+  await blogObject.save()
+  blogObject = new Blog(initalBlogs[3])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -11,16 +50,17 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 }, 100000)
 
-test('there are four blogs', async () => {
+test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(4)
+  expect(response.body).toHaveLength(initalBlogs.length)
 })
 
-test('the first blog is about React patterns', async () => {
+test('a specific blog is within the returned blogs', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body[0].title).toBe('React patterns')
+  const titles = response.body.map((r) => r.title)
+  expect(titles).toContain('React patterns')
 })
 
 afterAll(() => {
