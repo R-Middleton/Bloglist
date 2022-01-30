517fc2df3e6ae09983cfbce18d053a33d93cb1fb
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-01-30 18:50:44 +0000

    Added express-async-errors and refactored try/catch out of tests

--- controller/blogs.js
+++ controller/blogs.js
@@ -16,34 +16,23 @@ blogsRouter.post('/', async (request, response, next) => {
     url: body.url,
     likes: body.likes,
   })
-  try {
-    const savedBlog = await blog.save()
-    response.json(savedBlog.toJSON())
-  } catch (exception) {
-    next(exception)
-  }
+
+  const savedBlog = await blog.save()
+  response.json(savedBlog.toJSON())
 })
 
 blogsRouter.get('/:id', async (request, response, next) => {
-  try {
-    const blog = await Blog.findById(request.params.id)
-    if (blog) {
-      response.json(blog.toJSON())
-    } else {
-      response.status(404).end()
-    }
-  } catch (exception) {
-    next(exception)
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog.toJSON())
+  } else {
+    response.status(404).end()
   }
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch (exception) {
-    next(exception)
-  }
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
